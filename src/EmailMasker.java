1d87fdc3232b8a74ec777d1bf02a625e53124f9a
rfetalveroiii <rfetalvero@exist.com>
2017-05-02 13:28:54 +0800

    change variable for at index:x

--- src/EmailMasker.java
+++ src/EmailMasker.java
@@ -1,8 +1,8 @@
 public class EmailMasker {
     public String maskEmail(String email) {
-        int atIndex = email.indexOf('@');
+        int beforeAtIndexPos = email.indexOf('@') - 1;
         StringBuilder sb = new StringBuilder(email);
-        for (int i = 1; i < atIndex - 1; i++) {
+        for (int i = 1; i < beforeAtIndexPos; i++) {
             if (sb.charAt(i) != '.') {
                 sb.setCharAt(i, '*');
             }
