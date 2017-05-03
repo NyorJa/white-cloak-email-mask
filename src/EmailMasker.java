bdabe17d647cae62380dca7e9f64894d26248994
rfetalveroiii <rfetalvero@exist.com>
2017-05-03 09:31:55 +0800

    change getting indexOf @sign character

--- src/EmailMasker.java
+++ src/EmailMasker.java
@@ -1,7 +1,7 @@
 public class EmailMasker {
     public String maskEmail(String email) {
-        int beforeAtIndexPos = email.indexOf('@') - 1;
         StringBuilder sb = new StringBuilder(email);
+        int beforeAtIndexPos = sb.indexOf("@") - 1;
         for (int i = 1; i < beforeAtIndexPos; i++) {
             if (sb.charAt(i) != '.') {
                 sb.setCharAt(i, '*');
