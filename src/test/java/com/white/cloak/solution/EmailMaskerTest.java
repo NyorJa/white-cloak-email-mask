5b7bae1bbe5b54dca9b0e2cef8c336991a777f3e
NyorJa <botsot.felix@gmail.com>
2018-07-31 23:37:50 +0800

    update unit test

--- src/test/java/com/white/cloak/solution/EmailMaskerTest.java
+++ src/test/java/com/white/cloak/solution/EmailMaskerTest.java
@@ -2,14 +2,28 @@ package com.white.cloak.solution;
 
 import static junit.framework.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class EmailMaskerTest {
 
+    private EmailMasker emailMasker;
+
+    @Before
+    public void setUp() {
+        emailMasker = new EmailMasker();
+    }
+
+    @After
+    public void after() {
+        emailMasker = null;
+    }
+
+
     @Test
     public void itShouldMaskEmailWithAsteriskChars() {
-        EmailMasker emailMasker = new EmailMasker();
             assertEquals("it should mask with the same output",
             "a**.**d@gmail.com", emailMasker.maskEmail("anc.asd@gmail.com"));
     }
